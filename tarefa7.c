03e6a4f7f6861955d16270a611ba3fec622377c7
kevini78 <133727543+kevini78@users.noreply.github.com>
2023-08-16 22:51:37 -0300

    Add files via upload

---
+++ tarefa7.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int anos, meses, dias;
+    
+    printf("Digite a idade em anos: ");
+    scanf("%d", &anos);
+    
+    printf("Digite a idade em meses: ");
+    scanf("%d", &meses);
+    
+    printf("Digite a idade em dias: ");
+    scanf("%d", &dias);
+    
+    int idadeEmDias = (anos * 365) + (meses * 30) + dias;
+    
+    printf("A idade em dias é: %d\n", idadeEmDias);
+    
+   
+}
+   
